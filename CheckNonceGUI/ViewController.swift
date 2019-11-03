a6249708c3d079c793c47a4332906be566ec1cc6
matty <48230305+MatthewPierson@users.noreply.github.com>
2019-11-03 15:31:38 +1300

    Added output to see the generator of the shsh file or "null" if there is no generator

--- CheckNonceGUI/ViewController.swift
+++ CheckNonceGUI/ViewController.swift
@@ -21,7 +21,6 @@ class ViewController: NSViewController {
         alert.runModal()
         view.window?.level = .floating
         
-        
 
         // Do any additional setup after loading the view.
     }
@@ -29,10 +28,11 @@ class ViewController: NSViewController {
     
     @IBOutlet weak var filename_field: NSTextField!
     
+    @IBOutlet weak var apnonce_field: NSTextField!
     
     @IBAction func helpButton(_ sender: NSButton) {
         let alert = NSAlert.init()
-        alert.messageText = "Version 0.0.6"
+        alert.messageText = "Version 0.0.7"
         alert.informativeText = "Just a simple GUI for my Checkm8 Nonce Setter. Is mostly written in Swift, besides the stuff that interacts with the device as I am way too retarded to remake that in Swift. This is my first attempt at Swift so expect it to be broken and rubbish.\n\nCurrent device support is:\n\niPhone 5s, iPhone 7/7 Plus, iPhone X\niPad Mini 2, iPad Mini 3, iPad Air,\niPad 6th Gen, iPad 7th Gen\niPod Touch 7th Gen\n\nJust run each button in order and follow any prompts that pop up.\n\nIf the app looks frozen during the irecovery stuff, don't worry, it's most likely fine just freezes while it waits for irecovery to do its thing."
         alert.addButton(withTitle: "Go Back")
         alert.runModal()
@@ -56,12 +56,16 @@ class ViewController: NSViewController {
             if (result != nil) {
                 let path = result!.path
                 filename_field.stringValue = path
-                    print("SHSH location is:", path)
+                print("SHSH location is:", path)
+                var finalgenerator = getGenerator(shshPath: path)
+                apnonce_field.stringValue = finalgenerator
                     
             }
             
         } else {
             print("Why'd you cancel me =(")
+            let path = "Why'd you cancel me =("
+            filename_field.stringValue = path
             return
         }
         
@@ -79,6 +83,53 @@ class ViewController: NSViewController {
         do {
           try process.run()
         } catch {}
+    }
+    
+    func getGenerator(shshPath: String) -> String {
+        if shshPath != nil {
+        do {
+                let contents = try String(contentsOfFile: shshPath)
+                var noGenerator = contents.contains("generator")
+                if noGenerator == true {
+                    print("SHSH contains generator, is valid for this tool")
+                    let resourceFileDictionary = NSDictionary(contentsOfFile: shshPath)
+                    let s = resourceFileDictionary?.object(forKey: "generator")
+                    guard let finalGenerator: String = resourceFileDictionary?.object(forKey: "generator") as? String else {
+                        return "null"
+                    }
+                    
+                } else {
+                    print("Invalid SHSH, no generator found")
+                    let alert = NSAlert.init()
+                    alert.messageText = "Error"
+                    alert.informativeText = "No generator found. Please pick an SHSH file with a generator."
+                    alert.addButton(withTitle: "Go Back")
+                    alert.runModal()
+                    return "null"
+                }
+            } catch {
+                print("Couldn't find shsh")
+                let alert = NSAlert.init()
+                alert.messageText = "Error"
+                alert.informativeText = "No SHSH selected. Please locate your SHSH file and try again"
+                alert.addButton(withTitle: "Go Back")
+                alert.runModal()
+               
+            }
+        } else {
+            print("SHSH path not set properly. Please try again.")
+            let alert = NSAlert.init()
+            alert.messageText = "Error"
+            alert.informativeText = "Somehow you managed to select something that isn't a .shsh or .shsh2 file even though you shouldn't be able to. Please tell me how tf you did that"
+            alert.addButton(withTitle: "Go Back")
+            alert.runModal()
+        }
+        let resourceFileDictionary = NSDictionary(contentsOfFile: shshPath)
+        let s = resourceFileDictionary?.object(forKey: "generator")
+        guard let finalGenerator: String = resourceFileDictionary?.object(forKey: "generator") as? String else {
+            return "null"
+        }
+        return finalGenerator
     }
 
     
@@ -100,51 +151,15 @@ class ViewController: NSViewController {
         print("Getting generator from SHSH ")
         let shshLocation = filename_field.stringValue
         
-        if shshLocation != nil {
-            do {
-                let contents = try String(contentsOfFile: shshLocation)
-                var noGenerator = contents.contains("generator")
-                if noGenerator == true {
-                    print("SHSH contains generator, is valid for this tool")
-                    let resourceFileDictionary = NSDictionary(contentsOfFile: shshLocation)
-                    let s = resourceFileDictionary?.object(forKey: "generator")
-                    guard let finalGenerator: String = resourceFileDictionary?.object(forKey: "generator") as? String else {
-                        return
-                    }
-                    
-                } else {
-                    print("Invalid SHSH, no generator found")
-                    let alert = NSAlert.init()
-                    alert.messageText = "Error"
-                    alert.informativeText = "No generator found. Please pick an SHSH file with a generator."
-                    alert.addButton(withTitle: "Go Back")
-                    alert.runModal()
-                    return
-                }
-            } catch {
-                print("Couldn't find shsh")
-                let alert = NSAlert.init()
-                alert.messageText = "Error"
-                alert.informativeText = "No SHSH selected. Please locate your SHSH file and try again"
-                alert.addButton(withTitle: "Go Back")
-                alert.runModal()
-               
-            }
-        } else {
-            print("SHSH path not set properly. Please try again.")
-            let alert = NSAlert.init()
-            alert.messageText = "Error"
-            alert.informativeText = "Somehow you managed to select something that isn't a .shsh or .shsh2 file even though you shouldn't be able to. Please tell me how tf you did that"
-            alert.addButton(withTitle: "Go Back")
-            alert.runModal()
-        }
-        let resourceFileDictionary = NSDictionary(contentsOfFile: shshLocation)
-        let s = resourceFileDictionary?.object(forKey: "generator")
-        guard let finalGenerator: String = resourceFileDictionary?.object(forKey: "generator") as? String else {
+        var finalgenerator = getGenerator(shshPath: shshLocation)
+        if finalgenerator == "null" {
+            print("No generator found")
             return
+        } else {
+            print("Your generator is: \(finalgenerator)")
         }
        
-        irecoveryStuff(argu: "-c \"setenv com.apple.System.boot-nonce \(finalGenerator)\"")
+        irecoveryStuff(argu: "-c \"setenv com.apple.System.boot-nonce \(finalgenerator)\"")
         alert.messageText = "iRecovery stuff pt2"
         alert.informativeText = "Set generator"
         alert.runModal()
